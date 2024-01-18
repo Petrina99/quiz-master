0bdd191ccba709a6854690af3350c69fab2a2843
juraj_lovric <julovric@gmail.com>
2024-01-18 21:34:57 +0100

    added list

--- quizmaster/quiz/urls.py
+++ quizmaster/quiz/urls.py
@@ -5,5 +5,6 @@ app_name = "quiz"
 
 urlpatterns = [
     path('register', views.register_user, name="register_user"),
-    path('login', views.login_user, name="login_user")
+    path('login', views.login_user, name="login_user"),
+    path('quiz_list', views.quiz_list, name='quiz_list'),
 ]
