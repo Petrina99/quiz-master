afe09ddafb3ffd7432e36f3541c20c775cfc575c
juraj_lovric <julovric@gmail.com>
2024-01-23 19:10:00 +0100

    comments

--- quizmaster/quiz/urls.py
+++ quizmaster/quiz/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('<int:quiz_id>/delete/', views.delete_quiz, name="delete_quiz"),
     path('<int:quiz_id>/question/new', views.create_question, name="create_question"),
     path('<int:question_id>/question/delete/', views.delete_question, name="delete_question"),
-    path('<int:question_id>/question/', views.question_detail, name="question_detail")
+    path('<int:question_id>/question/', views.question_detail, name="question_detail"),
+    path('<int:quiz_id>/comments/new/', views.post_comment, name="post_comment"),
 ]
