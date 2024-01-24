3ec11025f8243262b4d81864af4925486f3dc5e0
juraj_lovric <julovric@gmail.com>
2024-01-24 17:23:27 +0100

    like

--- quizmaster/quiz/models.py
+++ quizmaster/quiz/models.py
@@ -9,6 +9,13 @@ class Quiz(models.Model):
     
     def __str__(self):
         return self.quiz_name
+    
+    def toggle_like(self, user):
+        if self.likes.filter(user=user).count() > 0:
+            like = self.likes.filter(user=user)
+            like.delete()
+        else:
+            like = self.likes.create(user=user)
    
 class Question(models.Model):
     question_text = models.CharField(max_length=200, blank=False)
